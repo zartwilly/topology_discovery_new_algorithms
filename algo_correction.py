7b62775ec9c31bfda3ef493515efab36c96d0f72
zartwilly <zartwilly@gmail.com>
2019-05-17 16:16:57 +0200

    correction_algo: A CODER.

---
+++ algo_correction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 16:05:31 2019
+
+@author: willy
+"""
+
+def correction_algo(cliques_couverture,
+                    aretes_LG_k_alpha,
+                    sommets_LG):
+    """
+    algorithme de correction 
+    cliques_couverture : cliques trouvees + aretes non supprimees
+    aretes_LG_k_alpha : aretes de mat_LG initial
+    sommets_LG : sommets(ou dico de sommets) ayant les caracteristiques modifiees
+    """
+    #TODO a coder
+    
+    return cliques_couverture, aretes_LG_k_alpha, sommets_LG;
